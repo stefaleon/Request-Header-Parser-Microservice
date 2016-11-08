c4b2c94ce7a311564e346ab6e225131103536f5e
LeonS <stefaleon@gmail.com>
2016-11-08 17:35:31 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/api/whoami', function(req, res) {
+
+	var ip = req.ip;
+	var lang = (req.headers['accept-language'].split(','))[0];
+	var sw = req.headers['user-agent'].split('(')[1].split(')')[0];
+
+	var output = {
+		"ipaddress": ip,
+		"language": lang,
+		"software": sw
+	}
+	res.send(output);
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
+	console.log('Server started!');
+})
